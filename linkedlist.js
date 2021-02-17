2e653d2e35d44f1e35f0b6f36741527e3d431413
evantwidwell <evanski@gmail.com>
2021-02-17 16:50:19 -0600

    more methods

--- linkedlist.js
+++ linkedlist.js
@@ -13,4 +13,28 @@ class LinkedList {
   insertFirst(data){
     this.head = new Node(data, this.head) 
   }
+  size(){
+    let counter = 0;
+    let node = this.head;
+    while(node){
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+  getFirst(){
+    return this.head
+  }
+  getLast(){
+    if(!this.head){
+      return null
+    }
+    let node = this.head;
+    while(node){
+      if(node.next === null){
+        return node
+      }
+      node = node.next;
+    }
+  }
 }
